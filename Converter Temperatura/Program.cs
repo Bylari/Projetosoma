2b1b7348ebe79e2415615ac844f7d0dda67b66cf
Bylari <larissa.braga2008@yahoo.com>
2025-09-15 22:15:26 -0300

    Add files via upload

---
+++ Converter Temperatura/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter_Temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // 1° Passo - declarar as variaveis
+            float temperaturaC, temperaturaF;
+            // 2/ Passo - Receber oa valores (ENTRADA)
+            Console.WriteLine("Conversão de Temperatura");
+            Console.Write("Entre com a temperatura em C°:");
+          temperaturaC = float.Parse( Console.ReadLine());
+
+            //3 Passo - Processamento
+            temperaturaF = (9 * temperaturaC + 160) / 5;
+
+            // 4 ° Passo - Saída 
+            Console.Write("A temperatura em fahrenheit é:" + temperaturaF);
+
+                Console.ReadKey();
+
+        }
+    }
+}
